03c8414d68e3d5234c98552129964d7111fc2bda
Vincent Barrilliot <vincent.barrilliot@sfr.fr>
2023-03-31 23:24:35 +0200

    Missing VBCC config files for A2560x

--- vbcc/targets/m68k-foenix/vlink_flash_a2560x.cmd
+++ vbcc/targets/m68k-foenix/vlink_flash_a2560x.cmd
@@ -1,11 +1,11 @@
 BINFILE_START = 0x00000000;
-FLASH_START = 0xFFC10000;
+FLASH_START = 0xFFC00000;
 VECTOR_START = 0x00000000;
-STORE_START = 0x00180000;
-KERNEL_START = 0x001C0000;
+STORE_START = 0x003D0000;
+KERNEL_START = 0xFFC10000;
 
 STACK_LEN = 0x400;
-VECTOR_LEN = 0x400;
+VECTOR_LEN = 0x1000;
 STORE_LEN = 0x00030000;
 KERNEL_LEN = 0x00040000;
 FLASH_LEN = 0x00200000;
@@ -13,21 +13,24 @@ PAGE_SIZE = 0x00010000;
 
 MEMORY
 {
-  binpage0: org = BINFILE_START, len = PAGE_SIZE
+  binpage0: org = BINFILE_START, len = VECTOR_LEN
+  binpage1: org = BINFILE_START + VECTOR_LEN, len = PAGE_SIZE - VECTOR_LEN
   binpages: org = BINFILE_START + PAGE_SIZE, len = FLASH_LEN - PAGE_SIZE
   vectors : org = VECTOR_START, len = VECTOR_LEN
+  lowram : org = VECTOR_START + VECTOR_LEN, len = PAGE_SIZE - VECTOR_LEN
   kernel: org = KERNEL_START, len = KERNEL_LEN
-  storeage: org = STORE_START, len = STORE_LEN
+  storage: org = STORE_START, len = STORE_LEN
 }
 
 SECTIONS
 {
-  vectors : { *(VECTORS) } >vectors
-  bss (NOLOAD) : { *(BSS) } >storeage
-  text ALIGN(0x02) : { *(CODE) } >kernel
-  .dtors ALIGN(0x02) : { *(.dtors) } >kernel
-  .ctors ALIGN(0x02) : { *(.ctors) } >kernel
-  data ALIGN(0x02) : { *(DATA) } >kernel
+  vectors : { *(VECTORS) } >vectors AT>binpage0
+  data ALIGN(0x02) : { *(DATA) } >lowram AT>binpage1
+  bss (NOLOAD) : { *(BSS) } >storage
+  text ALIGN(0x02) : { *(CODE) } >kernel AT>binpages
+  .dtors ALIGN(0x02) : { *(.dtors) } >kernel AT>binpages
+  .ctors ALIGN(0x02) : { *(.ctors) } >kernel AT>binpages
+
 
   ___heap = ADDR(bss) + SIZEOF(bss);
   ___heapend = STORE_START + STORE_LEN - STACK_LEN;
