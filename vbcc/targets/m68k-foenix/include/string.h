fffa26230fa974082b9dce48f38fee9a167f1ab1
Vincent Barrilliot <vincent.barrilliot@sfr.fr>
2023-11-03 00:26:50 +0100

    Some things are doubly-defined... weird that such problem is in VBCC ? We could disable inlining with __NOINLINE__ to prevent all that...?

--- vbcc/targets/m68k-foenix/include/string.h
+++ vbcc/targets/m68k-foenix/include/string.h
@@ -9,24 +9,24 @@ typedef unsigned long size_t;
 #undef NULL
 #define NULL ((void *)0)
 
-void *memcpy(void *,const void *,size_t);
+//void *memcpy(void *,const void *,size_t);
 void *memmove(void *,const void *,size_t);
-char *strcpy(char *,const char *);
+//char *strcpy(char *,const char *);
 char *strncpy(char *,const char *,size_t);
-char *strcat(char *,const char *);
-char *strncat(char *,const char *,size_t);
+//char *strcat(char *,const char *);
+//char *strncat(char *,const char *,size_t);
 int memcmp(const void *,const void *,size_t);
-int strcmp(const char *,const char *);
-int strncmp(const char *,const char *,size_t);
+//int strcmp(const char *,const char *);
+//int strncmp(const char *,const char *,size_t);
 void *memchr(const void *,int,size_t);
 char *strchr(const char *,int);
 size_t strcspn(const char *,const char *);
 char *strpbrk(const char *,const char *);
-char *strrchr(const char *,int);
+//char *strrchr(const char *,int);
 size_t strspn(const char *,const char *);
 char *strstr(const char *,const char *);
-void *memset(void *,int,size_t);
-size_t strlen(const char *);
+//void *memset(void *,int,size_t);
+//size_t strlen(const char *);
 char *strtok(char *,const char *);
 char *strerror(int);
 int strcoll(const char *,const char *);
