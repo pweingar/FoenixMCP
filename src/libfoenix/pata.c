f4484a60ed4405e8191e95804a085e611c8eafb9
vinz6751 <vincent.barrilliot@laposte.net>
2024-01-01 17:43:21 +0100

    Code cleanup

--- src/libfoenix/pata.c
+++ src/libfoenix/pata.c
@@ -150,7 +150,7 @@ int16_t pata_wait_data_request() {
     }
 }
 
-char g_buffer[512];
+int8_t g_buffer[512];
 
 //
 // Identify the PATA drive
@@ -162,9 +162,9 @@ char g_buffer[512];
 //  0 on success, any negative number is an error code
 //
 int16_t pata_identity(p_drive_info drive_info) {
-    char * buffer = 0;
+    int8_t * buffer = 0;
     uint16_t *wptr;
-    char * cptr;
+    int8_t * cptr;
     int16_t i;
     int16_t count;
     TRACE("pata_identity");
@@ -497,64 +497,4 @@ int16_t pata_flush() {
     TRACE("pata_flush");
     return 0;
 }
-
-//
-// Issue a control command to the PATA hard drive
-//
-// Inputs:
-//  command = the number of the command to send
-//  buffer = pointer to chars of additional data for the command
-//  size = the size of the buffer
-//
-// Returns:
-//  0 on success, any negative number is an error code
-//
-int16_t pata_ioctrl(int16_t command, uint8_t * buffer, int16_t size) {
-    int16_t result;
-    int32_t *p_long;
-    uint16_t *p_word;
-    int32_t *p_lba_word;
-    t_drive_info drive_info;
-    p_drive_info p_info;
-
-    TRACE("pata_ioctrl");
-
-    switch (command) {
-        case PATA_GET_SECTOR_COUNT:
-            p_lba_word = (int32_t *)buffer;
-            result = pata_identity(&drive_info);
-            if (result != 0) {
-                return result;
-            }
-
-            *p_lba_word = drive_info.l.lba_default;
-            break;
-
-        case PATA_GET_SECTOR_SIZE:
-            // Return the size of a sector... always 512
-            p_word = (uint16_t *)buffer;
-            *p_word = PATA_SECTOR_SIZE;
-            break;
-
-        case PATA_GET_BLOCK_SIZE:
-            // This isn't a flash device... return 1
-            p_long = (int32_t *)buffer;
-            *p_long = 1;
-            break;
-
-        case PATA_GET_DRIVE_INFO:
-            p_info = (p_drive_info)buffer;
-            result = pata_identity(p_info);
-            if (result != 0) {
-                return result;
-            }
-            break;
-
-        default:
-            return 0;
-    }
-
-    return 0;
-}
-
 
